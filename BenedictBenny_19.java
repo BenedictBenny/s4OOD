f89d3004c613b4dde199a5d72ea87037949e044f
BenedictBenny <47866379+BenedictBenny@users.noreply.github.com>
2019-02-22 00:15:15 +0530

    Benedict Benny 19

---
+++ BenedictBenny_19.java
@@ -0,0 +1,32 @@
+public class BenedictBenny_19
+{
+
+     
+	public static void main(String []args)
+	{
+        
+		for(int j=0;j<8;j++)
+		
+{
+
+        		for(int i=0;i<3;i++)
+			         System.out.print("X");
+
+		        System.out.println("");
+        
+		}
+
+         	for(int j=0;j<2;j++)
+		{
+
+        		for(int i=0;i<12;i++)
+			
+         System.out.print("X");
+             
+			System.out.println("");
+         
+		}
+
+        }
+
+}
